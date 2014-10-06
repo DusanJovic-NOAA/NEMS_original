DusanJovic-NOAA/NEMS_original

commit 7b6367b77964560fa6e50ef40459d1c90bba1985

Trunk update - Few minor bug fixes in NMMB nesting module (by Thiago Quirino, AOML); fixed loop boundaries in horizontal diffusion routine; updated configure files for Linux (adding NETCDF variables); constant rwind changed from 1./3.0 to 1./2.6 in NMMB polar filtering routine (also by Thiago).
git-svn-id: https://svnemc.ncep.noaa.gov/projects/nems/trunk@45178 281fb692-3a18-0410-826d-9e847983f365

src/atmos/nmm/module_DYNAMICS_ROUTINES.F90
@@ -2795,8 +2795,8 @@ real(kind=kfpt),dimension(ims:ime,jms:jme):: &
       vertical_loop_3: do l=1,lm
 !-----------------------------------------------------------------------
         if(l.gt.lpt2.and..not.hydro) then
-          do j=jts_b1,jte_h2
-            do i=its_b1,ite_h2
+          do j=jts_b1,jte_b1
+            do i=its_b1,ite_b1
               cilinx=sice(i-1,j).ne.sice(i,j) &
                  .or.sm  (i-1,j).ne.sm  (i,j)
               ciliny=sice(i,j-1).ne.sice(i,j) &

job/regression_tests/RT.sh_IN
@@ -2267,27 +2267,27 @@ else
  #
  ###############################################################################
 
- if [ $RT_FULL = true -o $CB_arg = nmm -o $CB_arg = all ]; then
-
-  export TEST_DESCR="Test NMMB-global with GFS physics package "
-
-#---------------------
-  (( TEST_NR=TEST_NR+1 ))
-  export RUNDIR=${RUNDIR_ROOT}/NMM_gfsP
-  export CNTL_DIR=NMMB_gfsP_glob
-  export LIST_FILES=" \
-  nmmb_hst_01_bin_0000h_00m_00.00s nmmb_hst_01_bin_0024h_00m_00.00s \
-  nmmb_hst_01_nio_0000h_00m_00.00s nmmb_hst_01_nio_0024h_00m_00.00s \
-  nmmb_rst_01_bin_0012h_00m_00.00s nmmb_rst_01_nio_0012h_00m_00.00s"
-#---------------------
-  export_nmm
-  export GBRG=glob ; export gfsP=true ; export FCSTL=24
-#---------------------
-   ./rt_nmm.sh
-   if [ $? = 2 ]; then exit ; fi
-#---------------------
-
- fi # endif test
+# if [ $RT_FULL = true -o $CB_arg = nmm -o $CB_arg = all ]; then
+#
+#  export TEST_DESCR="Test NMMB-global with GFS physics package "
+#
+##---------------------
+#  (( TEST_NR=TEST_NR+1 ))
+#  export RUNDIR=${RUNDIR_ROOT}/NMM_gfsP
+#  export CNTL_DIR=NMMB_gfsP_glob
+#  export LIST_FILES=" \
+#  nmmb_hst_01_bin_0000h_00m_00.00s nmmb_hst_01_bin_0024h_00m_00.00s \
+#  nmmb_hst_01_nio_0000h_00m_00.00s nmmb_hst_01_nio_0024h_00m_00.00s \
+#  nmmb_rst_01_bin_0012h_00m_00.00s nmmb_rst_01_nio_0012h_00m_00.00s"
+##---------------------
+#  export_nmm
+#  export GBRG=glob ; export gfsP=true ; export FCSTL=24
+##---------------------
+#   ./rt_nmm.sh
+#   if [ $? = 2 ]; then exit ; fi
+##---------------------
+#
+# fi # endif test
 
  ###############################################################################
  #
@@ -2478,27 +2478,27 @@ else
  #
  ###############################################################################
 
- if [ $ST_test = true -o $RT_FULL = true -o $CB_arg = nmm -o $CB_arg = all ] ; then
-
-  export TEST_DESCR="Test NMMB-regional with GFS physics package "
-
-#---------------------
-  (( TEST_NR=TEST_NR+1 ))
-  export RUNDIR=${RUNDIR_ROOT}/NMM_REG_gfsP
-  export CNTL_DIR=NMMB_gfsP_reg
-  export LIST_FILES=" \
-  nmmb_hst_01_bin_0000h_00m_00.00s nmmb_hst_01_bin_0024h_00m_00.00s \
-  nmmb_hst_01_nio_0000h_00m_00.00s nmmb_hst_01_nio_0024h_00m_00.00s \
-  nmmb_rst_01_bin_0012h_00m_00.00s nmmb_rst_01_nio_0012h_00m_00.00s"
-#---------------------
-  export_nmm
-  export GBRG=reg ; export gfsP=true ; export FCSTL=24
-#---------------------
-   ./rt_nmm.sh
-   if [ $? = 2 ]; then exit ; fi
-#---------------------
-
- fi # endif test
+# if [ $ST_test = true -o $RT_FULL = true -o $CB_arg = nmm -o $CB_arg = all ] ; then
+#
+#  export TEST_DESCR="Test NMMB-regional with GFS physics package "
+#
+##---------------------
+#  (( TEST_NR=TEST_NR+1 ))
+#  export RUNDIR=${RUNDIR_ROOT}/NMM_REG_gfsP
+#  export CNTL_DIR=NMMB_gfsP_reg
+#  export LIST_FILES=" \
+#  nmmb_hst_01_bin_0000h_00m_00.00s nmmb_hst_01_bin_0024h_00m_00.00s \
+#  nmmb_hst_01_nio_0000h_00m_00.00s nmmb_hst_01_nio_0024h_00m_00.00s \
+#  nmmb_rst_01_bin_0012h_00m_00.00s nmmb_rst_01_nio_0012h_00m_00.00s"
+##---------------------
+#  export_nmm
+#  export GBRG=reg ; export gfsP=true ; export FCSTL=24
+##---------------------
+#   ./rt_nmm.sh
+#   if [ $? = 2 ]; then exit ; fi
+##---------------------
+#
+# fi # endif test
 
  ###############################################################################
  #

src/atmos/nmm/module_FLTBNDS.F90
@@ -81,7 +81,7 @@ logical(kind=klog),allocatable,dimension(:) :: &
 
 real(kind=kfpt),parameter :: &
  cxnc=0.0 &
-,rwind=1./3.0 &
+,rwind=1./2.6 &
 ,cfilt=1.
 
 integer(kind=kint),intent(in) :: &

src/atmos/nmm/module_NESTING.F90
@@ -11695,12 +11695,12 @@
 !
       INTEGER(kind=KINT) :: IERR,ISTAT
 !
-      INTEGER(kind=KINT),DIMENSION(:),ALLOCATABLE :: COL,ROW
-!
       INTEGER(kind=KINT),DIMENSION(1:2) :: DIM_IDS
 !
       REAL(kind=KFPT) :: GBL,REAL_I_NE,REAL_I_SW,REAL_J_NE,REAL_J_SW    &
                         ,VAL_NE
+!
+      REAL(kind=KFPT),DIMENSION(:),ALLOCATABLE :: COL,ROW
 !
       CHARACTER(len=2) :: ID_TOPO_FILE
       CHARACTER(len=9) :: FILENAME
@@ -11878,17 +11878,17 @@
 !
         DO J=JSTART,JEND-1
         DO I=ISTART,IEND-1
-          NEST_FIS_V_ON_PARENT(N)%DATA(I,J)=(NEST_FIS_ON_PARENT(N)%DATA(I,J)   &
-                                            +NEST_FIS_ON_PARENT(N)%DATA(I+1,J) &
-                                            +NEST_FIS_ON_PARENT(N)%DATA(I,J+1) &
-                                            +NEST_FIS_ON_PARENT(N)%DATA(I,J+1) &
+          NEST_FIS_V_ON_PARENT(N)%DATA(I,J)=(NEST_FIS_ON_PARENT(N)%DATA(I,J)     &
+                                            +NEST_FIS_ON_PARENT(N)%DATA(I+1,J)   &
+                                            +NEST_FIS_ON_PARENT(N)%DATA(I,J+1)   &
+                                            +NEST_FIS_ON_PARENT(N)%DATA(I+1,J+1) &
                                             )*0.25
         ENDDO
         ENDDO
 !                
 !-----------------------------------------------------------------------
 !***  The V row at J=J_END is north of the H row at J=J_END.  
-!***  The V column at I=I_END is east of the H row at I=I_END.
+!***  The V column at I=I_END is east of the H column at I=I_END.
 !***  This means we need to read in extra values to get those 
 !***  V points on the north and east edges of the parent tasks.
 !-----------------------------------------------------------------------
@@ -11912,8 +11912,8 @@
                                  ,count=(/I_COUNT_DATA,1/)))               !    on this parent task.
 !
           DO I=ISTART,IEND-1
-            NEST_FIS_V_ON_PARENT(N)%DATA(I,JEND)=(NEST_FIS_ON_PARENT(N)%DATA(I,JEND-1)    &
-                                                  +NEST_FIS_ON_PARENT(N)%DATA(I+1,JEND-1) &
+            NEST_FIS_V_ON_PARENT(N)%DATA(I,JEND)=(NEST_FIS_ON_PARENT(N)%DATA(I,JEND)    &
+                                                  +NEST_FIS_ON_PARENT(N)%DATA(I+1,JEND) &
                                                   +ROW(I)+ROW(I+1) )*0.25
           ENDDO
 !
@@ -11938,9 +11938,9 @@
                                  ,count=(/1,J_COUNT_DATA/)))               !    on this parent task.
 !
           DO J=JSTART,JEND-1
-            NEST_FIS_V_ON_PARENT(N)%DATA(IEND,J)=(NEST_FIS_ON_PARENT(N)%DATA(IEND,J) &
-                                                  +NEST_FIS_ON_PARENT(N)%DATA(I,J+1) &
-                                                  +COL(J)+COL(J+1) )*0.25
+            NEST_FIS_V_ON_PARENT(N)%DATA(IEND,J)=(NEST_FIS_ON_PARENT(N)%DATA(IEND,J)   &
+                                                 +NEST_FIS_ON_PARENT(N)%DATA(IEND,J+1) &
+                                                 +COL(J)+COL(J+1) )*0.25
           ENDDO
 !
         ELSE
@@ -11962,6 +11962,10 @@
 !
           NEST_FIS_V_ON_PARENT(N)%DATA(IEND,JEND)=(NEST_FIS_ON_PARENT(N)%DATA(IEND,JEND) &
                                                     +ROW(IEND)+COL(JEND)+VAL_NE)*0.25
+        ENDIF
+!
+        IF(ITE==IDE.AND.JTE==JDE)THEN
+          NEST_FIS_V_ON_PARENT(N)%DATA(IEND,JEND)=0.
         ENDIF
 !
 !-----------------------------------------------------------------------

src/conf/configure.nems.Linux.gnu
@@ -8,7 +8,8 @@ gfsdir=atmos/$(gfs)
 
 NWPROD_LIB ?= $(TOP)/../../lib
 
-NETCDF_LIB  =   $(NWPROD_LIB)/netcdf/lib/libnetcdf.a
+NETCDF_INC  = -I$(NWPROD_LIB)/netcdf/include
+NETCDF_LIB  = -L$(NWPROD_LIB)/netcdf/lib -lnetcdff -lnetcdf
 
 NEMSIO_INC  = -I$(NWPROD_LIB)/incmod/nemsio
 NEMSIO_LIB  = -L$(NWPROD_LIB) -lnemsio
@@ -32,10 +33,10 @@ FREE        = -ffree-form
 FIXED       = -ffixed-form
 R8          = -fdefault-real-8
 
-FINCS       = $(ESMF_INC) $(NEMSIO_INC)
+FINCS       = $(ESMF_INC) $(NEMSIO_INC) $(NETCDF_INC)
 TRAPS       =
 
-FFLAGS      = $(TRAPS) $(FINCS) -fconvert=big-endian -fno-range-check -ffree-line-length-256
+FFLAGS      = $(TRAPS) $(FINCS) -fopenmp -fconvert=big-endian -fno-range-check -ffree-line-length-256
 
 OPTS_NMM    = -O3
 OPTS_GFS    = -O3

commit 7f664fd46265bc14b29b0bc16692b5695e3bf5c9

1.  Fix to shortwave RRTMG to allow running with TRAPS compiled in. Fixes 	    floating point exceptions reported by Dusan and Lin. 	2.  Fix an OpenMP threading error that was introduced in r45178 in 	    module_DYNAMICS_ROUTINES.F90 .  Fix is to revert two ending DO loop             ranges back to and ite_h2 and jte_h2.        (3.  change to configure.nems.savoy.ifort affects only me; savoy is in my house)
git-svn-id: https://svnemc.ncep.noaa.gov/projects/nems/trunk@46389 281fb692-3a18-0410-826d-9e847983f365

src/atmos/nmm/module_DYNAMICS_ROUTINES.F90
@@ -2795,8 +2795,8 @@ real(kind=kfpt),dimension(ims:ime,jms:jme):: &
       vertical_loop_3: do l=1,lm
 !-----------------------------------------------------------------------
         if(l.gt.lpt2.and..not.hydro) then
-          do j=jts_b1,jte_b1
-            do i=its_b1,ite_b1
+          do j=jts_b1,jte_h2
+            do i=its_b1,ite_h2
               cilinx=sice(i-1,j).ne.sice(i,j) &
                  .or.sm  (i-1,j).ne.sm  (i,j)
               ciliny=sice(i,j-1).ne.sice(i,j) &

src/atmos/phys/grrad_nmmb.f
@@ -1049,9 +1049,10 @@
       real (kind=kind_phys)::raddt,es(CHK),qs, delt, tem0d              &
      & ,SFCALBEDO(CHK), SMX(CHK)
 
+      integer j2
       integer :: i, j, k, k1, lv, icec, itop, ibtc, nday, idxday(CHK),   &
-     &       mbota(CHK,3), mtopa(CHK,3), LP1, nb, LMK, LMP, kd, lla, llb, &
-     &       lya, lyb, kt, kb, np3d
+     &      mbota(CHK,3), mtopa(CHK,3), LP1, nb, LMK, LMP, kd, lla, llb, &
+     &      lya, lyb, kt, kb, np3d
 
 !  ---  for debug test use
 !     real (kind=kind_phys) :: temlon, temlat, alon, alat
@@ -1065,7 +1066,7 @@
 !
       INTEGER :: IQ,JX(CHK)
 
-integer,external :: omp_get_thread_num
+      INTEGER,EXTERNAL :: omp_get_thread_num
 
  !     REAL (kind=kind_phys) :: ALBD0, ALVD1, ALND1
       REAL (kind=kind_phys) :: ZEN, DZEN, ALB1, ALB2
@@ -1789,6 +1790,40 @@ integer,external :: omp_get_thread_num
         if (isday) then
 
 !     print *,' in grrad : calling swrad'
+
+
+!jm pad out vector
+#if 1
+        if ( nday < CHK ) then
+          coszen_loc(nday+1:CHK) = coszen_loc(nday)
+          do k = 1,lmp
+            plvl   (nday+1:CHK,k)  = plvl  (nday,k)
+            tlvl   (nday+1:CHK,k)  = tlvl  (nday,k)
+          enddo
+          do k = 1,lmk
+            plyr   (nday+1:CHK,k)  = plyr  (nday,k)
+            tlyr   (nday+1:CHK,k)  = tlyr  (nday,k)
+            qlyr   (nday+1:CHK,k)  = qlyr  (nday,k)
+            olyr   (nday+1:CHK,k)  = olyr  (nday,k)
+          enddo
+          do j = 1,4
+            sfcalb (nday+1:CHK,j)  = sfcalb  (nday,j)
+          enddo
+          do j = 1,9
+            do k = 1,lmk
+              gasvmr    (nday+1:CHK,k,j)  = gasvmr  (nday,k,j)
+              clouds    (nday+1:CHK,k,j)  = clouds  (nday,k,j)
+            enddo
+          enddo
+          do j = 1,3
+            do j2 = 1,nbdsw
+              do k = 1,lmk
+                faersw (nday+1:CHK,k,j2,j)  = faersw (nday,k,j2,j)
+              enddo
+            enddo
+          enddo
+        endif
+#endif
 
           if ( present(htrswb) ) then
 

src/atmos/phys/radsw_main_nmmb.f
@@ -647,27 +647,6 @@
 !
 !===> ... begin here
 
-#if 0
-      write(0,*)__LINE__,'nday NDAY ',nday,NDAY
-      SHOWSHAPE(__LINE__,plyr)
-      SHOWSHAPE(__LINE__,plvl)
-      SHOWSHAPE(__LINE__,tlyr)
-      SHOWSHAPE(__LINE__,tlvl)
-      SHOWSHAPE(__LINE__,qlyr)
-      SHOWSHAPE(__LINE__,olyr)
-      SHOWSHAPE(__LINE__,gasvmr)
-      SHOWSHAPE(__LINE__,clouds)
-      SHOWSHAPE(__LINE__,icseed)
-      SHOWSHAPE(__LINE__,aerosols)
-      SHOWSHAPE(__LINE__,sfcalb)
-      SHOWSHAPE(__LINE__,cosz)
-      SHOWSHAPE(__LINE__,solcon)
-      SHOWSHAPE(__LINE__,hswc)
-      SHOWSHAPE(__LINE__,topflx)
-      SHOWSHAPE(__LINE__,sfcflx)
-      SHOWSHAPE(__LINE__,fdncmp)
-#endif
-
 !
       IF ( NDAY .GT.  CHK      ) THEN
         write(0,*)'radsw_main: swrad aborting NDAY ',NDAY,             &
@@ -735,7 +714,7 @@
 # define NDAY CHK
 #endif
 
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
         cosz1(j1)  = cosz(j1)
         sntz1(j1)  = f_one / cosz(j1)
         ssolar(j1) = s0fac * cosz(j1)
@@ -757,7 +736,7 @@
 
           do k = 1, nlay
             kk = nlp1 - k
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
             pavel(j1,k) = plyr(j1,kk)
             tavel(j1,k) = tlyr(j1,kk)
             delp (j1,k) = plvl(j1,kk+1) - plvl(j1,kk)
@@ -791,7 +770,7 @@
           if (iswrgas > 0) then
             do k = 1, nlay
               kk = nlp1 - k
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               colamt(j1,k,4)=                                           &
      &           max(temcol(j1,k),coldry(j1,k)*gasvmr(j1,kk,2))  ! n2o
               colamt(j1,k,5)=                                           &
@@ -804,7 +783,7 @@
             enddo
           else
             do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               colamt(j1,k,4) = temcol(j1,k)                                  ! n2o
               colamt(j1,k,5) = temcol(j1,k)                                  ! ch4
               colamt(j1,k,6) = temcol(j1,k)                                  ! o2
@@ -818,7 +797,7 @@
           do k = 1, nlay
             kk = nlp1 - k
             do ib = 1, nbdsw
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               tauae(j1,k,ib) = aerosols(j1,kk,ib,1)
               ssaae(j1,k,ib) = aerosols(j1,kk,ib,2)
               asyae(j1,k,ib) = aerosols(j1,kk,ib,3)
@@ -829,7 +808,7 @@
           if (iswcliq > 0) then    ! use prognostic cloud method
             do k = 1, nlay
               kk = nlp1 - k
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               cfrac(j1,k) = clouds(j1,kk,1)      ! cloud fraction
               cliqp(j1,k) = clouds(j1,kk,2)      ! cloud liq path
               reliq(j1,k) = clouds(j1,kk,3)      ! liq partical effctive radius
@@ -844,7 +823,7 @@
           else                     ! use diagnostic cloud method
             do k = 1, nlay
               kk = nlp1 - k
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               cfrac(j1,k) = clouds(j1,kk,1)      ! cloud fraction
               cdat1(j1,k) = clouds(j1,kk,2)      ! cloud optical depth
               cdat2(j1,k) = clouds(j1,kk,3)      ! cloud single scattering albedo
@@ -859,7 +838,7 @@
           tem2 = 1.0e-20 * 1.0e3 * con_avgd
 
           do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
             pavel(j1,k) = plyr(j1,k)
             tavel(j1,k) = tlyr(j1,k)
             delp (j1,k) = plvl(j1,k) - plvl(j1,k+1)
@@ -903,7 +882,7 @@
 
           if (iswrgas > 0) then
             do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               colamt(j1,k,4) =                                          &
      &            max(temcol(j1,k), coldry(j1,k)*gasvmr(j1,k,2))  ! n2o
               colamt(j1,k,5) =                                          &
@@ -916,7 +895,7 @@
       ENDDO
           else
             do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               colamt(j1,k,4) = temcol(j1,k)                                 ! n2o
               colamt(j1,k,5) = temcol(j1,k)                                 ! ch4
               colamt(j1,k,6) = temcol(j1,k)                                 ! o2
@@ -929,7 +908,7 @@
 
           do ib = 1, nbdsw
             do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               tauae(j1,k,ib) = aerosols(j1,k,ib,1)
               ssaae(j1,k,ib) = aerosols(j1,k,ib,2)
               asyae(j1,k,ib) = aerosols(j1,k,ib,3)
@@ -939,7 +918,7 @@
 
           if (iswcliq > 0) then    ! use prognostic cloud method
             do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               cfrac(j1,k) = clouds(j1,k,1)       ! cloud fraction
               cliqp(j1,k) = clouds(j1,k,2)       ! cloud liq path
               reliq(j1,k) = clouds(j1,k,3)       ! liq partical effctive radius
@@ -953,7 +932,7 @@
             enddo
           else                     ! use diagnostic cloud method
             do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
               cfrac(j1,k) = clouds(j1,k,1)       ! cloud fraction
               cdat1(j1,k) = clouds(j1,k,2)       ! cloud optical depth
               cdat2(j1,k) = clouds(j1,k,3)       ! cloud single scattering albedo
@@ -971,7 +950,7 @@
 
         if (iovrsw == 0) then                    ! random overlapping
           do k = 1, nlay
-      DO j1 = 1,nday
+      DO j1 = 1,NDAY
             zcf0(j1) = zcf0(j1) * (f_one - cfrac(j1,k))
       ENDDO
           enddo
@@ -1038,26 +1017,6 @@
 !  --- ...  calculate optical depths for gaseous absorption and Rayleigh
 !           scattering
 
-#if 0
-      write(0,*)__LINE__,'nday NDAY ',nday,NDAY
-      SHOWSHAPE(__LINE__,colamt)
-      SHOWSHAPE(__LINE__,colmol)
-      SHOWSHAPE(__LINE__,fac00)
-      SHOWSHAPE(__LINE__,fac01)
-      SHOWSHAPE(__LINE__,fac10)
-      SHOWSHAPE(__LINE__,fac11)
-      SHOWSHAPE(__LINE__,jp)
-      SHOWSHAPE(__LINE__,jt)
-      SHOWSHAPE(__LINE__,jt1)
-      SHOWSHAPE(__LINE__,laytrop)
-      SHOWSHAPE(__LINE__,forfac)
-      SHOWSHAPE(__LINE__,forfrac)
-      SHOWSHAPE(__LINE__,indfor)
-      SHOWSHAPE(__LINE__,selffrac)
-      SHOWSHAPE(__LINE__,selffrac)
-      SHOWSHAPE(__LINE__,indself)
-#endif
-
         call taumol                                                     &
 !  ---  inputs:
      &     ( colamt,colmol,fac00,fac01,fac10,fac11,jp,jt,jt1,laytrop,   &
@@ -1067,27 +1026,6 @@
      &     )
 
 !  --- ...  call the 2-stream radiation transfer model
-
-#if 0
-      write(0,*)__LINE__,'nday NDAY ',nday,NDAY
-      SHOWSHAPE(__LINE__,cosz1)
-      SHOWSHAPE(__LINE__,sntz1)
-      SHOWSHAPE(__LINE__,zcf1)
-      SHOWSHAPE(__LINE__,zcf0)
-      SHOWSHAPE(__LINE__,albbm)
-      SHOWSHAPE(__LINE__,albdf)
-      SHOWSHAPE(__LINE__,ssolar)
-      SHOWSHAPE(__LINE__,cldfrc)
-      SHOWSHAPE(__LINE__,sfluxzen)
-      SHOWSHAPE(__LINE__,taug)
-      SHOWSHAPE(__LINE__,taur)
-      SHOWSHAPE(__LINE__,taucw)
-      SHOWSHAPE(__LINE__,ssacw)
-      SHOWSHAPE(__LINE__,asycw)
-      SHOWSHAPE(__LINE__,tauae)
-      SHOWSHAPE(__LINE__,ssaae)
-      SHOWSHAPE(__LINE__,asyae)
-#endif
 
         if ( isubcsw <= 0 ) then     ! use standard cloud scheme
           call spcvrtc                                                  &
@@ -2382,7 +2320,7 @@
       SHOWSHAPE(__LINE__,ssaae)
       SHOWSHAPE(__LINE__,asyae)
 #endif
-#if 1
+#if 0
       if ( nday < CHK ) then
 !jm      write(0,*)__LINE__,'nday+1:CHK ',nday+1,CHK
         cosz  (nday+1:CHK)    = cosz (nday)

src/conf/configure.nems.savoy.ifort
@@ -9,6 +9,10 @@ gfsdir=atmos/$(gfs)
 
 #NETCDF_LIB  = /usrx/local/NetCDF/3.6.3/lib/libnetcdf.a
 
+NETCDF_INC  = -I$(NETCDF_INCLUDE)
+NETCDF_LIB  = -L$(NETCDF_LDFLAGS) -lnetcdff -lnetcdf
+
+
 NEMS_LIB_DATA=/home/michalak/NEMS-MIC/NEMS+LIBS_r19677/19677_libs
 NEMS_LIBS=/home/michalak/NEMS-MIC/NEMS+LIBS_r19677/19677_libs/../lib-snb
 NEMS_INC=/home/michalak/NEMS-MIC/NEMS+LIBS_r19677/19677_libs/../incmod-snb
@@ -65,7 +69,7 @@ FREE        = -free
 FIXED       = -fixed
 R8          = -r8
 
-FINCS       = $(ESMF_INC) $(NEMSIO_INC)
+FINCS       = $(ESMF_INC) $(NEMSIO_INC) $(NETCDF_INC)
 TRAPS       =
 #TRAPS       = -ftrapuv -check all -fp-stack-check -fpe0 -traceback
 #TRAPS       = -ftrapuv -check bounds -check format -check output_conversion -check pointers -check uninit -fp-stack-check